0b3f08e0f2e1d92e3820def5652be4fbde343bf8
Justyna Sadowska <korzyniewskaj@gmail.com>
2021-10-31 23:47:30 +0100

    11.3 booking - errors

--- src/js/settings.js
+++ src/js/settings.js
@@ -65,6 +65,12 @@ export const select = {
     peopleAmount: '.people-amount',
     hoursAmount: '.hours-amount',
     tables: '.floor-plan .table',
+    floorPlan: '.floor-plan',
+    form: '.booking-form',
+    submit: '.booking-form [type="submit"]',
+    phone: '.booking-form [name="phone"]',
+    address: '.booking-form [name="address"]',
+    starters: '.booking-form [name="starter"]',
   },
   nav: {
     links: '.main-nav a',
@@ -83,12 +89,16 @@ export const classNames = {
   },
 
   booking: {
-    peopleAmount: '.people-amount',
-    hoursAmount: '.hours-amount',
-    tables: '.floor-plan .table',
+    loading: 'loading',
+    table: 'table',
+    tableBooked: 'booked',
+    tableSelected: 'selected',
   },
   nav: {
-    links: '.main-nav a',
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
   },
   // CODE ADDED END
 };
@@ -121,8 +131,8 @@ export const settings = {
     url: '//localhost:3131',
     products: 'products',
     orders: 'orders',
-    booking: 'bookings',
-    event: 'events',
+    bookings: 'bookings',
+    events: 'events',
     dateStartParamKey: 'date_gte',
     dateEndParamKey: 'date_lte',
     notRepeatParam: 'repeat=false',
